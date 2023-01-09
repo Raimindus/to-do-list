4e5c4444790cc0603ed33e178fd979fa650d7402
Raimindus <draimindus@gmail.com>
2023-01-09 12:11:02 +0700

    glassmorph

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ function App() {
         <div className={styles.App}>
             <div className={styles.container}>
                 <div className={`${styles.form} ${styles.form1container}`}>
-                    <form className={styles.formContainer}><label>Input :</label><br /><input type="text" className={styles.input1} /><br /></form>
+                    <form className={styles.formContainer}>
+                        <label>Input :</label><br /><input type="text" className={styles.input1} /><br />
+                        <button className={styles.button1}>Input</button>
+                    </form>
                 </div>
                 <div className={styles.form2container}>
                     <div className={styles.listedItemContainer} />
